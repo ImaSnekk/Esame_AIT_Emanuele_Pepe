8e67d9a419eaf15ae2521a8d93af235122aaf572
Emanuele Pepe <148876479+ImaSnekk@users.noreply.github.com>
2024-10-27 18:07:49 +0100

    Update Installazione_EP.sh

--- Installazione_EP.sh
+++ Installazione_EP.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 # Crea una cartella in cui viene eseguito l'esercizio di Python
-mkdir Esame_Pepe
+mkdir -p Esame_Pepe
 
-# Copia il file
-cp Esame_AIT_EP.py Esame_AIT_EP_c.py
-cp Esecuzione_EP.sh Esecuzione_EP_c.sh
-
-# Sposta i file copiati nella cartella
-mv Esame_AIT_EP_c.py Esame_Pepe
-mv Esecuzione_EP_c.sh Esame_Pepe
+# Copia i file necessari nella nuova cartella
+cp Esame_AIT_EP.py Esame_Pepe/esameEP_c.py
+cp esecuzione_EP.sh Esame_Pepe/esecuzione_EP_c.sh
 
 # Entra nella cartella
 cd Esame_Pepe
 
 # Imposta la cartella base dove si trovano gli script
-APP_DIR=`pwd`
+APP_DIR=$(pwd)
 
 # Aggiunge la cartella Esame_Pepe al PYTHONPATH
 export PYTHONPATH=$APP_DIR:$PYTHONPATH
 
+# Aggiunge la cartella Esame_Pepe al PATH (se ci sono script eseguibili da chiamare direttamente)
+export PATH=$APP_DIR:$PATH
+
 # Aggiungi i permessi di esecuzione allo script Python e a quello Bash
-chmod +x $APP_DIR/Esame_AIT_EP_c.py
-chmod +x $APP_DIR/Esecuzione_EP_c.sh
+chmod +x esameEP_c.py
+chmod +x esecuzione_EP_c.sh
+
+# Conferma che il PATH e il PYTHONPATH sono stati aggiornati
+echo "Il PYTHONPATH è: $PYTHONPATH"
+echo "Il PATH è: $PATH"
+
